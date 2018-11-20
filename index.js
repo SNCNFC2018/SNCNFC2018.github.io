ec381e2d89ddae8b8abd0a56d1ab65925e85f509
SNCNFC2018 <45187020+SNCNFC2018@users.noreply.github.com>
2018-11-19 20:17:22 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+$(".button-collapse").sideNav({
+  menuWidth: 240, // Default is 240
+  edge: "left", // Choose the horizontal origin
+  closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+});
